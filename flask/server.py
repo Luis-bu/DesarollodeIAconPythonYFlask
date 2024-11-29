597bfee6f29db54a0fbd80ab6e769e02d47eaeef
luis-bu <luisbuemaxx@gmail.com>
2024-11-28 21:14:45 -0500

    pagina con flask

--- flask/server.py
+++ flask/server.py
@@ -1,12 +1,33 @@
-# Import the Flask class from the flask module
-from flask import Flask
+from flask import Flask, render_template, request
 
-# Create an instance of the Flask class, passing in the name of the current module
-app = Flask(__name__)
 
-# Define a route for the root URL ("/")
+app = Flask("Mathematics Problem Solver")
+
+@app.route("/sum")
+def sum_route():
+    num1 = float(request.args.get('num1'))
+    num2 = float(request.args.get('num2'))
+    result = summation(num1, num2)
+    return str(result)
+
+@app.route("/sub")
+def sub_route():
+    num1 = float(request.args.get('num1'))
+    num2 = float(request.args.get('num2'))
+    result = subtraction(num1, num2)
+    return str(result)
+
+@app.route("/mul")
+def mul_route():
+    num1 = float(request.args.get('num1'))
+    num2 = float(request.args.get('num2'))
+    result = multiplication(num1, num2)
+    return str(result)
+
 @app.route("/")
-def index():
-    # Function that handles requests to the root URL
-    # Return a plain text response
-    return "hello world"
+def render_index_page():
+    return render_template('index.html')
+
+    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
